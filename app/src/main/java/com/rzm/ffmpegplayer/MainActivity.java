96112db2ed0c44648c187fa2c10ee6b99b51b16f
renzhenming <renzhenming@58ganji.com>
2018-09-29 16:03:56 +0800

    add CMakeLists.txt

--- app/src/main/java/com/rzm/ffmpegplayer/MainActivity.java
+++ app/src/main/java/com/rzm/ffmpegplayer/MainActivity.java
@@ -24,6 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("ffmpeg");
     }
 }

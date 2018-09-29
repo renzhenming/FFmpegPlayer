2a321cae30cf51785f5e8e160663a0a6f96e4eb9
renzhenming <renzhenming@58ganji.com>
2018-09-29 17:46:33 +0800

    test success

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,10 @@
 #include <jni.h>
+
+//千万记住，ffmpeg是C语言编写的，在C++中使用必须开启开启混合编译，不然会一直报错，
+//undefined reference to 'xxxx'
+extern "C"{
 #include "libavcodec/avcodec.h"
+};
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_rzm_ffmpegplayer_MainActivity_stringFromJNI(

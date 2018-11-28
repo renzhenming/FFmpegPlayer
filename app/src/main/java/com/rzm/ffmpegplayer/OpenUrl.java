98253840564f560d6bcb95f5cf58824f41f1b2ef
renzhenming <1565766177@qq.com>
2018-11-29 07:55:44 +0800

    Update OpenUrl.java
    删除注释

--- app/src/main/java/com/rzm/ffmpegplayer/OpenUrl.java
+++ app/src/main/java/com/rzm/ffmpegplayer/OpenUrl.java
@@ -1,28 +1,3 @@
-/*******************************************************************************
- **                                                                            **
- **                     Jiedi(China nanjing)Ltd.                               **
- **	               创建：夏曹俊，此代码可用作为学习参考                       **
- *******************************************************************************/
-
-/*****************************FILE INFOMATION***********************************
- **
- ** Project       : FFmpeg
- ** Description   : FFMPEG项目创建示例
- ** Contact       : xiacaojun@qq.com
- **        博客   : http://blog.csdn.net/jiedichina
- **		视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun
- 腾讯课堂		https://jiedi.ke.qq.com/
- csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961
- **                 51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html
- ** 				   下载最新的ffmpeg版本 ffmpeg.club
- **
- **   安卓流媒体播放器 课程群 ：23304930 加入群下载代码和交流
- **   微信公众号  : jiedi2007
- **		头条号	 : 夏曹俊
- **
- *******************************************************************************/
-//！！！！！！！！！ 加群23304930下载代码和交流
-
 
 package com.rzm.ffmpegplayer;
 
